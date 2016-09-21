4b37fc0594cf3ba2862937dc8007cefc3cb76895
Chris Cowley <chris.cowley@groupe-regards.com>
2016-09-21 18:28:11 +0200

    Call R10k using subprocess

--- r10kwebhook/__init__.py
+++ r10kwebhook/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, request
 from  flask.ext.runner import Runner
 import json
+import subprocess
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
@@ -19,12 +20,13 @@ def deploy():
     elif request.method == 'POST':
         data = json.loads(request.data.decode('utf-8'))
         print("Deploying " + data['environment'])
-        os.system(
-                app.config['R10K'] + 
-                ' deploy environment ' + 
-                data['environment'] + 
-                ' -p -vv -c ' + 
-                app.config['R10K_CONF'] )
+        subprocess.call([
+                app.config['R10K'],
+                'deploy', 'environment',
+                data['environment'],
+                '-p', '-c',
+                app.config['R10K_CONF']
+                ], shell=True)
         return json.dumps({'msg': 'Hi!'})
 
 
