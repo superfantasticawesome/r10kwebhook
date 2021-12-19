cb1d36954cf7caffc21190f5b2d10b91ae1a7b54
Gerard Ryan <hello@superfantasticawesome.com>
2021-12-19 12:59:04 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import argparse
 from r10kwebhook import app
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', '-p', dest='port', default=5000)
+parser.add_argument('--port', '-p', dest='port', default=8088)
 parser.add_argument('--debug', '-d', dest='debug', default=True)
 parser.add_argument('--host', dest='host', default='127.0.0.1')
 args = parser.parse_args()
